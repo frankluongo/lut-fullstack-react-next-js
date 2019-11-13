4a729045a1570e0de9dc3e0d970565a6748d759f
Frank Luongo <frank@frankluongo.com>
2019-11-13 14:47:38 -0700

    12: CSS in NextJS

--- tracker/components/habit/habit.js
+++ tracker/components/habit/habit.js
@@ -2,16 +2,39 @@ import HabitButton from "./habitButton";
 
 const Habit = ({ habit }) => {
   const dates = getLast5Days();
-  console.log(dates);
   return (
-    <article>
-      <h3>{habit}</h3>
-      <div>
-        {dates.map((date, index) => (
-          <HabitButton date={date} key={index} />
-        ))}
-      </div>
-    </article>
+    <>
+      <article>
+        <h3>{habit}</h3>
+        <div>
+          {dates.map((date, index) => (
+            <HabitButton date={date} key={index} />
+          ))}
+        </div>
+      </article>
+      <style jsx>{`
+        article {
+          margin-bottom: 2rem;
+          padding: 1rem;
+
+          box-shadow: 0 1px 8px 2px rgba(0, 0, 0, 0.15);
+        }
+
+        h3 {
+          display: block;
+          margin-bottom: 1rem;
+          padding-bottom: 1rem;
+
+          text-transform: capitalize;
+
+          border-bottom: 1px solid red;
+        }
+
+        div {
+          display: flex;
+        }
+      `}</style>
+    </>
   );
 };
 
--- tracker/components/habit/habitButton.js
+++ tracker/components/habit/habitButton.js
@@ -9,10 +9,37 @@ const HabitButton = ({ date }) => {
   }
 
   return (
-    <div>
-      <div>{`${date.getMonth() + 1}/ ${date.getDate()}`}</div>
-      <button onClick={handleClick.bind(this)}>{indicator}</button>;
-    </div>
+    <>
+      <section>
+        <div>{`${date.getMonth() + 1}/ ${date.getDate()}`}</div>
+        <button onClick={handleClick.bind(this)}>{indicator}</button>
+      </section>
+      <style jsx>{`
+        section {
+          padding: 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+        }
+        div {
+          margin-bottom: 1rem;
+        }
+        button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          height: 2rem;
+          width: 2rem;
+          margin: 0;
+          padding: 0;
+
+          font-size: 1rem;
+
+          border: 1px solid #ccc;
+        }
+      `}</style>
+    </>
   );
 };
 
